482ced28112203920f3c18bbe42b8964f43e7e3a
Hailemichael Atrsaw <hailat.alx@gmail.com>
2022-03-14 15:44:38 +0300

    0x00-hello_world project done!

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: prints all possible combinations of two digit numbers
+ * by using only putchar.
+ * Return: returns 0
+ */
+int main(void)
+{
+	putchar(1 + '0');
+	return (0);
+}
