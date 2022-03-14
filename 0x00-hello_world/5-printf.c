5e3401f1fae18295013e7f9aad05f51f3fcff21e
Hailemichael Atrsaw <hailat.alx@gmail.com>
2022-03-14 11:52:15 +0300

    0x00-hello_world project done!

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+/**
+ * main - Entery point
+ *
+ * Description: prints exactly the string "with proper grammar,
+ * but the outcome is a piece of art,"
+ * Return: returns 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
