bc6a85be8a5051ff6715c9374af97f840cb4d695
Hailemichael Atrsaw <hailat.alx@gmail.com>
2022-03-14 14:05:05 +0300

    0x00-hello_world project done!

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,1 +1,24 @@
+#include <stdio.h>
 
+/**
+ * main - Entry point
+ *
+ * Decription: prints alphabet in lowercase and then in uppercase
+ * by only using putchar.
+ * Return: returns 0
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+	}
+	for (ch = 'A'; ch <= 'Z'; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
