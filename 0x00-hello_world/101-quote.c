b62a71d355c8ad75ead47a277e461447b687a249
Hailemichael Atrsaw <hailat.alx@gmail.com>
2022-03-14 12:17:01 +0300

    0x00-hello_world project done!

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,4 @@
-#include<stdio.h>
+#include<unistd.h>
 
 /**
  * main - Entry point
@@ -10,6 +10,5 @@
 int main(void)
 {
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 61);
-	
 	return (1);
 }
