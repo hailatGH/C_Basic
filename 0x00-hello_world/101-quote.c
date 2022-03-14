ddf0814a8f0a3d0ec415bed166b7b5cbe5832194
Hailemichael Atrsaw <hailat.alx@gmail.com>
2022-03-14 12:13:50 +0300

    0x00-hello_world project done!

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: prints exactly the string "and that piece of art is useful"
+ * - Dora Korpar, 2015-10-19" with out using printf and puts.
+ * Return: returns 1
+ */
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 61);
+	return (1);
+}
