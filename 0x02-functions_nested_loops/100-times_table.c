83a61b4f8a5acf9dff4aca0f6725a9ce38d4efd5
Hailemichael Atrsaw <hailat.alx@gmail.com>
2022-03-15 22:46:11 +0300

    0x02-functions_nested_loops project commits

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -10,36 +10,38 @@ void print_times_table(int n)
 {
 	int i, j, res;
 
-	if ((n <= 15) && (n >= 0))
+	if (!(n > 15 || n < 0))
 	{
 		for (i = 0; i <= n; i++)
 		{
 			for (j = 0; j <= n; j++)
 			{
-				res = i * j;
+				res = (i * j);
 				if (j != 0)
 				{
 					_putchar(',');
 					_putchar(' ');
 				}
-				if ((res >= 10) && (res < 100) && (j != 0))
+				if (res < 10 && j != 0)
+				{
+					_putchar(' ');
+					_putchar(' ');
+					_putchar((res % 10) + '0');
+				}
+				else if (res >= 10 && res < 100)
 				{
 					_putchar(' ');
 					_putchar((res / 10) + '0');
 					_putchar((res % 10) + '0');
 				}
-				else if ((res >= 100) && (j != 0))
+				else if (res >= 100 && j != 0)
 				{
-					_putchar(((res / 10) / 10) + '0');
-					_putchar(((res / 10) % 10) + '0');
+					_putchar((res / 100) + '0');
+					_putchar((res / 10) % 10 + '0');
 					_putchar((res % 10) + '0');
 				}
 				else
-				{
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(res + '0');
-				}
+					_putchar((res % 10) + '0');
 			}
 			_putchar('\n');
 		}
