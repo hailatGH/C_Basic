0aa057f56cbb2075da2e6432394c5350225d9337
Hailemichael Atrsaw <hailat.alx@gmail.com>
2022-03-15 22:08:42 +0300

    0x02-functions_nested_loops project commits

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add - adds two integers and returns the result
+ * @i: first arg
+ * @j: second arg
+ *
+ * Return: sum
+ */
+int add(int i, int j)
+{
+	return (i + j);
+}
+
