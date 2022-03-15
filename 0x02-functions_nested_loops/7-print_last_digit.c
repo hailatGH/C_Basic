09daadac358cd89432c687ad042eedd7456a6aae
Hailemichael Atrsaw <hailat.alx@gmail.com>
2022-03-15 21:19:39 +0300

    0x02-functions_nested_loops project commits

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -8,11 +8,10 @@
  */
 int print_last_digit(int n)
 {
-	int l;
+	n = n % 10;
 
 	if (n < 0)
-		n = n * -1;
-	l = n % 10;
-	_putchar(l + '0');
-	return (l);
+		n = -n;
+	_putchar(n + '0');
+	return (n);
 }
