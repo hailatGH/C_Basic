d55ca174e3991a8905f47efc5c778e6adead3d07
Hailemichael Atrsaw <hailat.alx@gmail.com>
2022-03-17 19:58:16 +0300

    commit changed cause of some issues.

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -14,11 +14,11 @@ void print_diagonal(int n)
 		_putchar('\n');
 	else
 	{
-		for (i = 0; i < n; i++)
+		for (i = 1; i <= n; i++)
 		{
 			if (i > 1)
 			{
-				for (j = 0; j <= i; j++)
+				for (j = 1; j <= i-1; j++)
 					_putchar(' ');
 			}
 			_putchar('\\');
