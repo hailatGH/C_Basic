7f2bca5ba8b78f927b074ed97243b44618d85da9
Hailemichael Atrsaw <hailat.alx@gmail.com>
2022-03-21 16:27:29 +0300

    Main projects are completed!

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -8,15 +8,13 @@
  */
 void puts_half(char *str)
 {
-	int len, n, i;
+	int len;
 
 	for (len = 0; str[len] != '\0'; len++)
 		;
+	len++;
 
-	if (len % 2 != 0)
-		len--;
-	n = len / 2;
-	for (i = n; i <= len; i++)
-		_putchar(str[i]);
+	for (len /= 2; str[len] != '\0'; len++)
+		_putchar(str[len]);
 	_putchar('\n');
 }
