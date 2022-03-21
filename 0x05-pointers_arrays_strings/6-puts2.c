6d706d77e3e0881e2de0dacbb14b9b0b899694dc
Hailemichael Atrsaw <hailat.alx@gmail.com>
2022-03-21 16:15:29 +0300

    Main projects are completed!

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -10,13 +10,13 @@
  */
 void puts2(char *str)
 {
-	int i;
+	int i = 0;
 
-	for (i = 0; str[i] != '\0'; i++)
+	while (str[i] != '\0')
 	{
-		if (i != 0)
-			i++;
-		_putchar(str[i]);
+		if (i % 2 == 0)
+			_putchar(str[i]);
+		i++;
 	}
 	_putchar('\n');
 }
