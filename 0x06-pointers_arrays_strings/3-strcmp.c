fe45da420cf27391b503fba98e0578fb22e84bc2
Hailemichael Atrsaw <hailat.alx@gmail.com>
2022-03-23 22:43:33 +0300

    Done the mendatory task

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -5,9 +5,7 @@
  * @s1: first string arg to be compared.
  * @s2: second string arg to be compared.
  *
- * Return: posetive number if s1 > s2
- * 	   zero is s1 == s2
- * 	   negative s1 < s2
+ * Return: posetive number if s1 > s2, zero is s1 == s2, negative s1 < s2
  */
 int _strcmp(char *s1, char *s2)
 {
