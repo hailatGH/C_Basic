b4a886255a8d32318615764d5d1dc42e5fa0ea82
Hailemichael Atrsaw <hailat.alx@gmail.com>
2022-03-23 23:42:29 +0300

    Done the mendatory task

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * rot13 - encodes a string into rot13
+ * @s: string to encode
+ *
+ * Return: address of s
+ */
+char *rot13(char *s1)
+{
+	int i, j;
+	char a[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char b[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+
+	for (i = 0; *(s1 + i); i++)
+	{
+		for (j = 0; j < 52; j++)
+		{
+			if (a[j] == *(s1 + i))
+			{
+				*(s1 + i) = b[j];
+				break;
+			}
+		}
+	}
+	return (s1);
+}
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,3 +1,4 @@
+int _putchar(char);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
@@ -6,3 +7,7 @@ void reverse_array(int *a, int n);
 char *string_toupper(char *s1);
 char *cap_string(char *s1);
 char *leet(char *s1);
+char *rot13(char *s1);
+void print_number(int n);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
+void print_buffer(char *b, int size);
