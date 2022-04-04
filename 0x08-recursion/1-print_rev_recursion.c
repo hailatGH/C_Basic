fc756748cd8f647109aff1f90a771c68497a576e
Hailemichael Atrsaw <hailat.alx@gmail.com>
2022-04-04 21:52:51 +0300

    0x08-recursion project commit

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - function that prints a string in reverse.
+ * @s: the string to be printed.
+ *
+ * Return: Nothing.
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return;
+	}
+	else
+	{
+		s++;
+		_print_rev_recursion(s);
+		s--;
+		_putchar(*s);
+	}
+}
--- 0x08-recursion/main.c
+++ 0x08-recursion/main.c
@@ -1,7 +1,12 @@
 #include "main.h"
-/** * main - check the code * * Return: Always 0. */
-int
-main(void) {
-    _puts_recursion("Puts with recursion");
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    _print_rev_recursion("\nColton Walker");
     return (0);
 }
