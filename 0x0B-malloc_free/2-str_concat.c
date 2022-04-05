b9890a28ee54d749e9754afb28fc23b618ce4bad
Hailemichael Atrsaw <hailat.alx@gmail.com>
2022-04-05 21:23:31 +0300

    0x0B-malloc_free project done

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -17,12 +17,18 @@ char *str_concat(char *s1, char *s2)
 	unsigned int count2 = 0;
 	char *new_str = NULL;
 
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 	while (*(s1 + i))
 		i++;
 	while (*(s2 + j))
 		j++;
 
 	new_str = malloc((i + j + 1) * sizeof(char));
+	if (new_str == NULL)
+		return (NULL);
 	if (new_str != NULL)
 	{
 		while (count < i)
