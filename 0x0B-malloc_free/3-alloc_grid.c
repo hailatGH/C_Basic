bf85a21734ce33582c6e2a101e76c8cfda747293
Hailemichael Atrsaw <hailat.alx@gmail.com>
2022-04-05 21:39:44 +0300

    0x0B-malloc_free project done

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,48 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * alloc_grid - function that returns a pointer to a 2 dimensional
+ * array of integers.
+ * @width: width of the array.
+ * @height: height of the array.
+ *
+ * Return: pointer to the new array.
+ */
+int **alloc_grid(int width, int height)
+{
+	int **matrix;
+	int i;
+	int j;
+	int l;
+	int *p;
+
+	if (width <= 0 || height <= 0)
+		return (NULL);
+	matrix = (int **)malloc(height * sizeof(int *));
+	if (matrix == NULL)
+		return (NULL);
+	for (i = 0; i < height; i++)
+	{
+		*(matrix + i) = (int *)malloc(width * sizeof(int));
+		if (*(matrix + i) == NULL)
+		{
+			for (i = 0; i < height; i++)
+			{
+				p = matrix[i];
+				free(p);
+			}
+			free(matrix);
+			return (NULL);
+		}
+	}
+	for (l = 0; l < height; l++)
+	{
+		for (j = 0; j < width; j++)
+		{
+			matrix[l][j] = 0;
+		}
+	}
+	return (matrix);
+}
